7fdd920687a2e184016b37a622b5a528b7104643
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-03-08 02:17:54 +0500

    append(value)

--- script.js
+++ script.js
@@ -11,4 +11,16 @@ class LinkedList {
         this.tail = null;
         this.size = 0;
     }
+
+    append(value){
+        const newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.size++;
+    }
 }
