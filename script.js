f622f4d331a1ade226c0c6d9fdc4b6080f5a9d97
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-03-08 02:09:30 +0500

    LinkedList and Node class

---
+++ script.js
@@ -0,0 +1,14 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.nextNode = null;
+    }
+}
+
+class LinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+}
